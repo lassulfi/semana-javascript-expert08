32a08129dc5924471e2047702f765a2c5d8f56ed
Luis Daniel Assulfi <lassulfi@gmail.com>
2023-10-29 10:08:43 -0300

    finished project

---
+++ app/pages/file-upload/src/view.js
@@ -0,0 +1,72 @@
+export default class View {
+    #fileUpload = document.getElementById('fileUpload')
+    #btnUploadVideo = document.getElementById('btnUploadVideos')
+    #fileSize = document.getElementById('fileSize')
+    #fileInfo = document.getElementById('fileInfo')
+    #txtfileName = document.getElementById('fileName')
+    #fileUploadWrapper = document.getElementById('fileUploadWrapper')
+    #elapsed = document.getElementById('elapsed')
+
+    /** @type {HTMLCanvasElement} */
+    #canvas = document.getElementById('preview-144p')
+
+    constructor() {
+        this.configureBtnUploadClick()
+    }
+
+    onChange(fn) {
+        return e => {
+            const file = e.target.files[0]
+            const { name, size } = file
+            fn(file)
+
+            this.#txtfileName.innerText = name
+            this.#fileSize.innerText = this.parseBytesIntoMBAndGB(size)
+
+            this.#fileInfo.classList.remove('hide')
+            this.#fileUploadWrapper.classList.add('hide')
+        }
+    }
+
+    parseBytesIntoMBAndGB(bytes) {
+        const mb = bytes / (1024 * 1024)
+        // if mb is greater than 1024, then convert to GB
+        if (mb > 1024) {
+            // rount to 2 decimal places
+            return `${Math.round(mb / 1024)}GB`
+        }
+        return `${Math.round(mb)}MB`
+    }
+
+    updateElapsedTime(text) {
+        this.#elapsed.innerText = text
+    }
+
+    configureOnFileChange(fn) {
+        this.#fileUpload.addEventListener('change', this.onChange(fn))
+    }
+
+    configureBtnUploadClick() {
+        this.#btnUploadVideo.addEventListener('click', () => {
+            // trigger file input
+            this.#fileUpload.click()
+        })
+    }
+
+    getCanvas() {
+        return this.#canvas.transferControlToOffscreen()
+    }
+
+    downloadBlobAsFile(buffers, fileName) {
+        const blob = new Blob(buffers, { type: 'video/webm' })
+        const blobUrl = URL.createObjectURL(blob)
+
+        const link = document.createElement('a')
+        link.href = blobUrl
+        link.download = fileName
+
+        link.click()
+
+        URL.revokeObjectURL(blobUrl)
+    }
+}
